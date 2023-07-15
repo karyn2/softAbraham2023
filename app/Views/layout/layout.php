72d517f1886ea321a63f42e96aafff020407aefb
karyn2 <karynaceron@gmail.com>
2023-07-15 10:34:23 -0500

    Cambios en layout usuario logueado, cerrar sesion2  @karyn2 15/07/2023

--- app/Views/layout/layout.php
+++ app/Views/layout/layout.php
@@ -74,7 +74,7 @@
                         @else -->
                             <li class="nav-item dropdown ">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    <!-- {{ Auth::user()->name }} -->
+                                    <?php echo session('nombre')?>
                                 </a>
 
                                 <div class=" dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink" id="muenudrop">
@@ -83,15 +83,11 @@
                                 <a class="dropdown-item" href="#">Configuración</a>
                                 
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        <!-- {{ __('Cerrar sesión') }} -->
-                                </a>
+                                <a class="dropdown-item" href="<?php echo base_url('/logout')?>">Cerrar sesión</a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        <!-- @csrf -->
-                                    </form>
+    
+
+                                   
                                 </div>
                             </li>
                         <!-- @endguest -->
