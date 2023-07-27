47e40223f9283bff0e256298522fa01c09a51694
vadelgado <vadelgado@udenar.edu.co>
2023-07-27 11:53:06 -0500

    push1

--- app/Models/usuarios.php
+++ app/Models/usuarios.php
@@ -8,10 +8,29 @@ class usuarios extends Model{
     protected $useAutoIncrement = true;
     protected $returnType       = 'array';
     protected $allowedFields    = ['documento', 'nombre','correo','contrasenia','rol','estado'];
-
+        // Relación uno a uno con la tabla 'profesores'
+    protected $hasOne = [
+           'profesor' => [
+           'model' => 'App\Models\ProfesoresModel',
+           'foreign_key' => 'usuario_id',
+            'local_key' => 'id_usuario'
+        ]
+    ];  
+    public function buscarPorDocumento($documento)
+    {
+        // Realiza la búsqueda del usuario por su número de documento y verifica el estado y el rol.
+        // Reemplaza 'documento' con el nombre del campo en tu tabla, y 'docente' con el valor del rol de docente.
+        return $this->where('documento', $documento)
+                    ->where('estado', 1)
+                    ->where('rol', 'docente')
+                    ->first();
+    }  
+   
     public function obtenerUsuario($data){
         $usuario = $this->db->table('usuarios');
         $usuario->where($data);
         return $usuario->get()->getResultArray();
     }
+
+
 }
